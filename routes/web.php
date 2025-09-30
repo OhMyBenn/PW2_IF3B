fa094fbf1bf828f52e2fa325ba5773bf1047a671
RubenAlfandoWhite <rubenalfandowhite2016@gmail.com>
2025-09-30 14:28:26 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\ProdiController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
 
 Route::get('/', function () {
     return view('welcome');
